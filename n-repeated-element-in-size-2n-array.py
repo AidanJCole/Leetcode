317797c689bdc6dcaa747d04d83b9484314cf522
Aidan Cole <aidanjcole@gmail.com>
2019-05-12 17:08:14 -0400

    N Repeated Elements in Size 2N Array

---
+++ n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedNTimes(self, A: List[int]) -> int:
+        seen = set()
+        for x in A:
+            if x in seen:
+                return x
+            else:
+                seen.add(x)
